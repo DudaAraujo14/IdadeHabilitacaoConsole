b7422200bc4d07c9484301832058025bd83bba41
DudaAraujo14 <rm560944@fiap.com.br>
2025-09-24 21:23:00 -0300

    up

---
+++ IdadeHabilitacaoConsole/IdadeHabilitacaoConsole/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+class Program
+{
+    // Struct para armazenar os dados do usuário
+    struct Pessoa
+    {
+        public string Nome;
+        public DateTime DataNascimento;
+
+        // Construtor da struct
+        public Pessoa(string nome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            DataNascimento = dataNascimento;
+        }
+
+        // Função para calcular a idade da pessoa
+        public int CalcularIdade()
+        {
+            int idade = DateTime.Now.Year - DataNascimento.Year;
+
+            // Ajuste caso a pessoa ainda não tenha feito aniversário este ano
+            if (DateTime.Now < DataNascimento.AddYears(idade))
+            {
+                idade--;
+            }
+
+            return idade;
+        }
+
+        // Função para verificar se a pessoa é maior de idade
+        public string VerificarMaiorIdade()
+        {
+            int idade = CalcularIdade();
+            if (idade >= 18)
+            {
+                return "Você é maior de idade e pode tirar a carteira de habilitação!";
+            }
+            else
+            {
+                return "Você é menor de idade e não está apto para tirar a habilitação ainda.";
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        // Entrada de dados
+        Console.WriteLine("Digite seu nome completo:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite sua data de nascimento (formato: dd/MM/yyyy):");
+        DateTime dataNascimento;
+        string dataInput = Console.ReadLine();
+
+        // Validar o formato da data
+        while (!DateTime.TryParseExact(dataInput, "dd/MM/yyyy", null, System.Globalization.DateTimeStyles.None, out dataNascimento))
+        {
+            Console.WriteLine("Data inválida. Por favor, insira a data no formato correto (dd/MM/yyyy):");
+            dataInput = Console.ReadLine();
+        }
+
+        // Instanciando a struct com os dados do usuário
+        Pessoa pessoa = new Pessoa(nome, dataNascimento);
+
+        // Usando as funções da struct
+        int idade = pessoa.CalcularIdade();
+        string mensagem = pessoa.VerificarMaiorIdade();
+
+        // Saída
+        Console.WriteLine($"\nOlá, {pessoa.Nome}!");
+        Console.WriteLine($"Sua idade é: {idade} anos.");
+        Console.WriteLine(mensagem);
+
+        Console.WriteLine("\nPressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
---
+++ IdadeHabilitacaoConsole/IdadeHabilitacaoConsole/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IdadeHabilitacaoConsole")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("FIAP")]
+[assembly: AssemblyProduct("IdadeHabilitacaoConsole")]
+[assembly: AssemblyCopyright("Copyright © FIAP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("754991df-5c31-4efe-9490-65ff1e888a36")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
